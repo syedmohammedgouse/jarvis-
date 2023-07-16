367a3d982b755852805c71618bcd8196371664f8
syed mohammed <85601222+syedmohammedgouse@users.noreply.github.com>
2023-07-17 00:40:01 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -9,7 +9,7 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
+print(voices[1].id)
 engine.setProperty('voice', voices[1].id)                   
 
 def speak(audio):
